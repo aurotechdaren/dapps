23829f09e54a51f33aab3131e8e38e2a06406b96
sreeharshachennuru <sreharsha@gmail.com>
2019-03-11 00:26:01 -0400

    Updated Funding model with properties

--- funding/src/models/funding.model.ts
+++ funding/src/models/funding.model.ts
@@ -8,11 +8,60 @@ export class Funding extends Entity {
     generated: true
   })
   id: string;
+  
   @property({
     type: 'string',
     required: true,
   })
   name: string;
+
+  @property({
+    type: 'string',
+    required: false,
+  })
+  cost: string;
+
+ @property({
+    type: 'string',
+    required: false,
+  })
+  tradeOffs: string;
+
+ @property({
+    type: 'string',
+    required: false,
+  })
+  priceTechnicalTradeOffAnalysis: string;
+
+ @property({
+    type: 'string',
+    required: false,
+  })
+  budgetingAndFunding: string;
+
+ @property({
+    type: 'string',
+    required: false,
+  })
+  estimateCurrentYearFunding: string;
+
+ @property({
+    type: 'string',
+    required: false,
+  })
+  isCompetitive: string;
+
+@property({
+    type: 'string',
+    required: false,
+  })
+  compatibility: string;
+
+  @property({
+    type: 'string',
+    required: false,
+  })
+  constraints: string;
 
 
   constructor(data?: Partial<Funding>) {
